9acf597d901605055234fca9f8cc95598b398bb1
Sam Kiehl <e22-kiehlsg@elderhs.net>
2022-12-12 19:51:48 -0500

    Initial Commit

---
+++ EditorWindow.java
@@ -0,0 +1,390 @@
+import java.io.File;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class EditorWindow{
+    private static ImageEditor ie;
+    private static JLabel im;
+    private static JFrame imageWindow;
+    private static JFrame frame;
+    private static JPanel panel;
+    private static TextField sx = new TextField();
+    private static TextField sy = new TextField();
+    private static TextField sa = new TextField();
+    private static TextField sr = new TextField();
+    private static TextField sg = new TextField();
+    private static TextField sb = new TextField();
+
+    private static TextField percent = new TextField();
+
+    private static ActionListener[] actions = {
+        new SetPixel(),
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+    };
+
+    public static void main (String[] args){  
+            ie = null;
+
+            imageWindow = new JFrame();
+
+            frame = new JFrame("Image Editor");
+            panel = new JPanel(); 
+            
+            frame.setVisible(true);
+            frame.setSize(1280,720);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            frame.add(panel);
+
+            JButton openButton = new JButton("Browse Files");
+            panel.add(openButton);
+            openButton.addActionListener (new Browse()); 
+
+            JButton setPixel = new JButton("Set Pixel");
+            panel.add(setPixel);
+            setPixel.addActionListener(new SetPixel());
+
+            JButton grayscale = new JButton("Grayscale");
+            panel.add(grayscale);
+            grayscale.addActionListener(new Grayscale());
+
+            JButton blackAndWhite = new JButton("Black & White");
+            panel.add(blackAndWhite);
+            blackAndWhite.addActionListener(new BlackAndWhite());
+
+            JButton adjustBrightness = new JButton("Adjust Brightness");
+            panel.add(adjustBrightness);
+            adjustBrightness.addActionListener(new AdjustBrightness());
+
+            JButton reverseContrast = new JButton("Reverse Contrast");
+            panel.add(reverseContrast);
+            reverseContrast.addActionListener(new ReverseContrast());
+
+            JButton deepfry = new JButton("Deepfry");
+            panel.add(deepfry);
+            deepfry.addActionListener(new Deepfry());
+
+            JButton generalize = new JButton("Generalize Colors");
+            panel.add(generalize);
+            generalize.addActionListener(new Generalize());
+
+            JButton generalizeRGB = new JButton("Generalize RGB");
+            panel.add(generalizeRGB);
+            generalizeRGB.addActionListener(new GeneralizeRGB());
+
+            JButton flipH = new JButton("Flip Horizontal");
+            panel.add(flipH);
+            flipH.addActionListener(new FlipH());
+
+            JButton flipV = new JButton("Flip Vertical");
+            panel.add(flipV);
+            flipV.addActionListener(new FlipV());
+
+            JButton mirror0 = new JButton("Mirror Top");
+            panel.add(mirror0);
+            mirror0.addActionListener(new Mirror0());
+
+            JButton mirror1 = new JButton("Mirror Bottom");
+            panel.add(mirror1);
+            mirror1.addActionListener(new Mirror1());
+
+            JButton mirror2 = new JButton("Mirror Left");
+            panel.add(mirror2);
+            mirror2.addActionListener(new Mirror2());
+
+            JButton mirror3 = new JButton("Mirror Right");
+            panel.add(mirror3);
+            mirror3.addActionListener(new Mirror3());
+
+            JButton save = new JButton("Save Image");
+            panel.add(save);
+            save.addActionListener(new Save());
+
+    }
+    
+    static class Browse implements ActionListener {        
+        public void actionPerformed(ActionEvent e) {
+            //Handle open button action.
+            final JFileChooser fc = new JFileChooser();
+            fc.setCurrentDirectory(new File(System.getProperty("user.dir")));
+            int returnVal = fc.showOpenDialog(null);
+    
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                ie = new ImageEditor(fc.getSelectedFile());
+                imageWindow = new JFrame(ie.getAbsolutePath());
+                im = new JLabel();
+                //This is where a real application would open the file.
+                System.out.println("Opening: " + ie.getName() + "."/* + newline*/);
+                addImage();
+            } else {
+                System.out.println("Open command cancelled by user."/* + newline*/);
+            }
+        }
+    }
+
+    public static void addImage(){ 
+        im.setIcon(null);
+        im.setVisible(true);
+        //ie.save(); // EVIL, CAUSED SO MANY PROBLEMS
+        imageWindow.setName(ie.getAbsolutePath());
+        imageWindow.setAlwaysOnTop(true);
+        System.out.println("Displaying: " + ie.getAbsolutePath());
+        imageWindow.setVisible(true);
+        imageWindow.add(im);
+        im.setIcon(null);
+        ImageIcon ii = new ImageIcon(ie.getAbsolutePath());
+        ii.getImage().flush();
+        im.setIcon(scaleDown(ii));
+        imageWindow.pack();
+        
+
+    }
+
+    public static void deleteImage(String path){
+        File f = new File("./Temp/" + ie.getName().substring(0, ie.getName().indexOf(".")) + "(edit)" + ie.getName().substring(ie.getName().indexOf(".")));
+        if (f.delete()) { 
+        System.out.println("Deleted the file: " + f.getName());
+        } else {
+        System.out.println("Failed to delete the file.");
+        } 
+    }
+
+
+    public static void onActionTrigger(Action a){
+        im.setIcon(new ImageIcon(ie.save().getAbsolutePath()));
+        
+    }
+
+    public static ImageIcon scaleDown(ImageIcon imageIcon){
+        int width = ie.getWidth();
+        int height = ie.getHeight();
+
+        if(width > 1280){
+            float scalar = 1280f / (float)width;
+            Image image = imageIcon.getImage(); // transform it 
+            Image newimg = image.getScaledInstance((int)(1280*scalar), (int)(720*scalar),  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+            imageIcon = new ImageIcon(newimg);  // transform it back
+        }
+        if(height > 720){
+            float scalar = 720f / (float)height;
+            Image image = imageIcon.getImage(); // transform it 
+            Image newimg = image.getScaledInstance((int)(1280*scalar), (int)(720*scalar),  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+            imageIcon = new ImageIcon(newimg);  // transform it back
+        }
+        return imageIcon;
+    }
+
+
+    static class SetPixel implements ActionListener { 
+        public void actionPerformed(ActionEvent e){
+            JFrame sp = new JFrame("Set Pixel");
+            sp.setVisible(true);
+            sp.setSize(300, 40);
+            JLabel label = new JLabel("Choose Coordinates");
+            JPanel p = new JPanel();
+
+            sx = new TextField("x", 5);
+            p.add(sx);
+            sy = new TextField("y", 5);
+            p.add(sy);
+            sa = new TextField("alpha", 3);
+            p.add(sa);
+            sr = new TextField("red", 3);
+            p.add(sr);
+            sg = new TextField("green", 3);
+            p.add(sg);
+            sb = new TextField("blue", 3);
+            p.add(sb);
+
+            sp.add(p);
+            p.add(label); 
+            
+
+            
+            JButton set = new JButton("Set");
+            p.add(set);
+            set.addActionListener (new Set());
+        }
+    }  
+    static class Set implements ActionListener { 
+        public void actionPerformed(ActionEvent ee){
+            int x = 0;
+            int y = 0;
+            int a = 0;
+            int r = 0;
+            int g = 0;
+            int b = 0;
+            System.out.println("sx.text={"+sx.getText()+"}");
+            try{ x = Integer.parseInt(sx.getText()); }
+            catch(Exception ex){ panel.add(new JLabel("Please enter a number.")); }
+            try{ y = Integer.parseInt(sy.getText()); }
+            catch(Exception ex){ panel.add(new JLabel("Please enter a number.")); } 
+            try{ a = Integer.parseInt(sa.getText()); }
+            catch(Exception ex){ panel.add(new JLabel("Please enter a number.")); }
+            try{ r = Integer.parseInt(sr.getText()); }
+            catch(Exception ex){ panel.add(new JLabel("Please enter a number.")); }
+            try{ g = Integer.parseInt(sg.getText()); }
+            catch(Exception ex){ panel.add(new JLabel("Please enter a number.")); }
+            try{ b = Integer.parseInt(sb.getText()); }
+            catch(Exception ex){ panel.add(new JLabel("Please enter a number.")); }
+            
+            ie.setPixel(x, y, a, r, g, b);
+            ie.save("./Temp/" + ie.getName().substring(0, ie.getName().indexOf(".")) + "(edit)" + ie.getName().substring(ie.getName().indexOf(".")));
+            addImage();
+        }
+    }
+
+    static class Grayscale implements ActionListener { 
+        public void actionPerformed(ActionEvent ee){
+            ie.grayscale();
+            ie.save("./Temp/" + ie.getName().substring(0, ie.getName().indexOf(".")) + "(edit)" + ie.getName().substring(ie.getName().indexOf(".")));
+            addImage();
+        }
+    }
+
+    static class BlackAndWhite implements ActionListener {
+        public void actionPerformed(ActionEvent ee){
+            ie.blackAndWhite();
+            ie.save("./Temp/" + ie.getName().substring(0, ie.getName().indexOf(".")) + "(edit)" + ie.getName().substring(ie.getName().indexOf(".")));
+            addImage();
+        }
+    }
+
+    static class AdjustBrightness implements ActionListener { 
+        public void actionPerformed(ActionEvent e){
+            JFrame ab = new JFrame("Adjust Brightness");
+            ab.setVisible(true);
+            ab.setSize(300, 40);
+            JLabel label = new JLabel("Choose Adjustment %");
+            JPanel p = new JPanel();
+
+            percent = new TextField("%", 5);
+            p.add(percent);
+
+            ab.add(p);
+            p.add(label); 
+            
+
+            
+            JButton apply = new JButton("Apply");
+            p.add(apply);
+            apply.addActionListener (new Apply());
+        }
+    }  
+    static class Apply implements ActionListener { 
+        public void actionPerformed(ActionEvent ee){
+            int perc = 0;
+            System.out.println("ab.text={"+percent.getText()+"}");
+            try{ perc = Integer.parseInt(percent.getText()); }
+            catch(Exception ex){ panel.add(new JLabel("Please enter a number.")); }
+            
+            ie.adjustBrightness(perc);
+            ie.save("./Temp/" + ie.getName().substring(0, ie.getName().indexOf(".")) + "(edit)" + ie.getName().substring(ie.getName().indexOf(".")));
+            addImage();
+        }
+    }
+
+    static class ReverseContrast implements ActionListener {
+        public void actionPerformed(ActionEvent ee){
+            ie.reverseContrast();
+            ie.save("./Temp/" + ie.getName().substring(0, ie.getName().indexOf(".")) + "(edit)" + ie.getName().substring(ie.getName().indexOf(".")));
+            addImage();
+        }
+    }   
+
+    static class Deepfry implements ActionListener {
+        public void actionPerformed(ActionEvent ee){
+            ie.deepfry();
+            ie.save("./Temp/" + ie.getName().substring(0, ie.getName().indexOf(".")) + "(edit)" + ie.getName().substring(ie.getName().indexOf(".")));
+            addImage();
+        }
+    }   
+
+    static class Generalize implements ActionListener {
+        public void actionPerformed(ActionEvent ee){
+            ie.generalize();
+            ie.save("./Temp/" + ie.getName().substring(0, ie.getName().indexOf(".")) + "(edit)" + ie.getName().substring(ie.getName().indexOf(".")));
+            addImage();
+        }
+    }
+
+    static class GeneralizeRGB implements ActionListener {
+        public void actionPerformed(ActionEvent ee){
+            ie.generalizeRGB();
+            ie.save("./Temp/" + ie.getName().substring(0, ie.getName().indexOf(".")) + "(edit)" + ie.getName().substring(ie.getName().indexOf(".")));
+            addImage();
+        }
+    }
+
+    static class FlipH implements ActionListener {
+        public void actionPerformed(ActionEvent ee){
+            ie.flipH();
+            ie.save("./Temp/" + ie.getName().substring(0, ie.getName().indexOf(".")) + "(edit)" + ie.getName().substring(ie.getName().indexOf(".")));
+            addImage();
+        }
+    }
+
+    static class FlipV implements ActionListener {
+        public void actionPerformed(ActionEvent ee){
+            ie.flipV();
+            ie.save("./Temp/" + ie.getName().substring(0, ie.getName().indexOf(".")) + "(edit)" + ie.getName().substring(ie.getName().indexOf(".")));
+            addImage();
+        }
+    }
+
+    static class Mirror0 implements ActionListener {
+        public void actionPerformed(ActionEvent ee){
+            ie.mirror0();
+            ie.save("./Temp/" + ie.getName().substring(0, ie.getName().indexOf(".")) + "(edit)" + ie.getName().substring(ie.getName().indexOf(".")));
+            addImage();
+        }
+    }
+
+    static class Mirror1 implements ActionListener {
+        public void actionPerformed(ActionEvent ee){
+            ie.mirror1();
+            ie.save("./Temp/" + ie.getName().substring(0, ie.getName().indexOf(".")) + "(edit)" + ie.getName().substring(ie.getName().indexOf(".")));
+            addImage();
+        }
+    }
+
+    static class Mirror2 implements ActionListener {
+        public void actionPerformed(ActionEvent ee){
+            ie.mirror2();
+            ie.save("./Temp/" + ie.getName().substring(0, ie.getName().indexOf(".")) + "(edit)" + ie.getName().substring(ie.getName().indexOf(".")));
+            addImage();
+        }
+    }
+
+    static class Mirror3 implements ActionListener {
+        public void actionPerformed(ActionEvent ee){
+            ie.mirror3();
+            ie.save("./Temp/" + ie.getName().substring(0, ie.getName().indexOf(".")) + "(edit)" + ie.getName().substring(ie.getName().indexOf(".")));
+            addImage();
+        }
+    }
+
+    static class Save implements ActionListener {
+        public void actionPerformed(ActionEvent ee){
+            ie.mirror0();
+            ie.save("./Temp/" + ie.getName().substring(0, ie.getName().indexOf(".")) + "(edit)" + ie.getName().substring(ie.getName().indexOf(".")));
+            ie.save();
+            System.out.println("Saving: " + ie.getAbsolutePath());
+            addImage();
+        }
+    }
+}
---
+++ ImageEditor.java
@@ -0,0 +1,269 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+public class ImageEditor{
+
+    private File f = null;
+    private BufferedImage img = null;
+    private String name = null;
+    private int width, height, p = 0;
+
+    public int getWidth(){ return width; }
+    public int getHeight(){ return height; }
+    public File getFile(){ return f; }
+    public BufferedImage getImg(){ return img; }
+    public String getName(){ return name; }
+    public String getAbsolutePath(){ return f.getAbsolutePath(); }
+
+    public void main(String args[])throws IOException{
+        // deepfry();
+        // generalize();
+        // generalizeRGB();
+        // reverseContrast();
+        // grayscale();
+        // flipH();
+        // flipV();
+        // adjustBrightness(275);
+        // blackAndWhite();
+        // mirror0();
+        // mirror1();
+        // mirror2();
+        // mirror3();
+        // reverseContrast();
+        // deepfry();
+        // mirror0();
+        // mirror0();
+
+        save();
+    }
+
+    public ImageEditor(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Image Filepath (include file extension): ");
+        this.name = scanner.nextLine();
+        scanner.close();
+        //read image
+        try{
+        f = new File("./Input Images/" + name);
+        img = ImageIO.read(f);
+        }catch(IOException e){ e.printStackTrace(); }
+
+        //get image width and height
+        width = img.getWidth();
+        height = img.getHeight();
+    }
+
+    public ImageEditor(File file){
+        img = null;
+        f = file;
+        name = f.getName();
+        //read image
+        try{
+            img = ImageIO.read(f);
+        }catch(IOException e){ e.printStackTrace(); }
+
+        //get image width and height
+        width = img.getWidth();
+        height = img.getHeight();
+    }
+
+    public int[] getRGBArr(int x, int y){
+        int rgb = img.getRGB(x, y);
+        int aa = (rgb >> 24) & 0xFF;
+        int red = (rgb >> 16) & 0xFF;
+        int green = (rgb >> 8) & 0xFF;
+        int blue = rgb & 0xFF;
+        int[] output = {aa, red, green, blue};
+        return output;
+    }
+    
+    public void setPixel(int x, int y, int a, int r, int g, int b){
+        p = img.getRGB(0,0);
+        p = (a<<24) | (r<<16) | (g<<8) | b;
+        img.setRGB(x, y, p);
+    }
+
+    public static int getGrayscaleValue(int r, int g, int b){ return (r + g + b) / 3; }
+
+    public void grayscale(){
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < height; j++){
+                int[] rgb = getRGBArr(i, j);
+                int gray = getGrayscaleValue(rgb[1], rgb[2], rgb[3]);
+                setPixel(i, j, rgb[0], gray, gray, gray);
+            }
+        }
+    }
+
+    public void blackAndWhite(){
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < height; j++){
+                int[] rgb = getRGBArr(i, j);
+                if(getGrayscaleValue(rgb[1], rgb[2], rgb[3]) > 127)
+                    setPixel(i, j, rgb[0], 255, 255, 255);
+                else
+                    setPixel(i, j, rgb[0], 0, 0, 0);
+
+            }
+        }
+    }
+
+    public void adjustBrightness(int percent){
+        float multiplier = 1 + percent/100;
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < height; j++){
+                int[] rgb = getRGBArr(i, j);
+                int r2 = (int)Math.max(0, Math.min(255, rgb[1] * multiplier));
+                int g2 = (int)Math.max(0, Math.min(255, rgb[2] * multiplier));
+                int b2 = (int)Math.max(0, Math.min(255, rgb[3] * multiplier));
+                setPixel(i, j, rgb[0], r2, g2, b2);
+            }
+        }
+    }
+
+    public void reverseContrast(){
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < height; j++){
+                int[] rgb = getRGBArr(i, j);
+                int r2 = 255 - rgb[1];
+                int g2 = 255 - rgb[2];
+                int b2 = 255 - rgb[3];
+                setPixel(i, j, rgb[0], r2, g2, b2);
+            }
+        }
+    }
+
+    public void deepfry(){
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < height; j++){
+                int[] rgb = getRGBArr(i, j);
+
+                int r2 = rgb[1];
+                if(rgb[1] > rgb[2] && rgb[1] > rgb[3])
+                    r2 = (int)Math.max(0, Math.min(255, rgb[1] * rgb[1]/2));
+                int g2 = (int)Math.max(0, Math.min(255, rgb[2] / 2));
+                int b2 = (int)Math.max(0, Math.min(255, rgb[3] / 2));
+                setPixel(i, j, rgb[0], r2, g2, b2);
+            }
+        }
+        adjustBrightness(50);
+    }
+
+    public void generalize(){
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < height; j++){
+                int[] rgb = getRGBArr(i, j);
+                int r2 = rgb[1];
+                int g2 = rgb[2];
+                int b2 = rgb[3];
+                if(r2 > g2 && r2 > b2)
+                    setPixel(i, j, rgb[0], r2, 0, 0);
+                else if(g2 > r2 && g2 > b2)
+                    setPixel(i, j, rgb[0], 0, g2, 0);
+                else if(b2 > r2 && b2 > g2)
+                    setPixel(i, j, rgb[0], 0, 0, b2);
+                else
+                    setPixel(i, j, rgb[0], 0, 0, 0);
+            }
+        }
+    }
+
+    public void generalizeRGB(){
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < height; j++){
+                int[] rgb = getRGBArr(i, j);
+                int r2 = rgb[1];
+                int g2 = rgb[2];
+                int b2 = rgb[3];
+                if(r2 > g2 && r2 > b2)
+                    setPixel(i, j, rgb[0], 255, 0, 0);
+                else if(g2 > r2 && g2 > b2)
+                    setPixel(i, j, rgb[0], 0, 255, 0);
+                else if(b2 > r2 && b2 > g2)
+                    setPixel(i, j, rgb[0], 0, 0, 255);
+                else
+                    setPixel(i, j, rgb[0], 0, 0, 0);
+            }
+        }
+    }
+
+    public void flip(boolean vertical){
+        int[][] pixels = new int[width][height];
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < height; j++){
+                pixels[vertical ? i : width - 1 - i][vertical ? height - 1 - j : j] = img.getRGB(i, j);
+            }
+        }
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < height; j++){
+                img.setRGB(i, j, pixels[i][j]);
+            }
+        }
+    }
+
+    public void flipH(){ flip(false); }
+
+    public void flipV(){ flip(true); }
+
+    public void mirror(int sector){
+        switch(sector){
+            case 0:
+                for(int i = 0; i < width; i++)
+                    for(int j = height/2; j >= 0; j--)
+                        img.setRGB(i, height -1 - j, img.getRGB(i, j));
+                break;
+            case 1:
+                for(int i = 0; i < width; i++)
+                    for(int j = height/2+1; j < height; j++)
+                        img.setRGB(i, height-1-j, img.getRGB(i, j));
+                break;
+            case 2:
+                for(int i = width/2; i >= 0; i--)
+                    for(int j = 0; j < height-1; j++)
+                        img.setRGB(width - 1 - i, j, img.getRGB(i, j));
+                break;
+            case 3:
+                for(int i = width/2+1; i < width; i++){
+                    for(int j = 0; j < height-1; j++){
+                        img.setRGB(width-1-i, j, img.getRGB(i, j));
+                    }
+                }
+                break;
+        }
+    }
+
+    public void mirror0(){ mirror(0); }
+    
+    public void mirror1(){ mirror(1); }
+
+    public void mirror2(){ mirror(2); }
+
+    public void mirror3(){ mirror(3); }
+
+    public File save(){
+        try{
+            f = new File("./Output Images/" + name.substring(0, name.indexOf(".")) + "(edit)" + name.substring(name.indexOf(".")));
+            ImageIO.write(img, "png", f);
+            img.flush();
+            return f;
+        }catch(IOException e){
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    public File save(String path){
+        try{
+            f = new File(path);
+            ImageIO.write(img, "png", f);
+            img.flush();
+            return f;
+        }catch(IOException e){
+            System.out.println(e);
+        }
+        return null;
+    }
+}
